d3b1c0360e661d940b01d4abd197c2237ab1876d
Haris Amjad <harisamjad56@gmail.com>
2025-09-08 02:58:31 +0500

    Fix Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,40 +3,61 @@ import { TbCopyCheckFilled } from "react-icons/tb";
 import React from "react";
 
 const mainLinks = [
-  { href: "/features", label: "Features" },
-  { href: "/ai", label: "AI Magic" },
-  { href: "/resources", label: "Resources" },
-  { href: "/about", label: "About" },
-  { href: "/contact", label: "Contact" },
+  { href: "/product", label: "Product", child: [] },
+  { href: "/ai", label: "AI Assistant", child: [] },
+  { href: "/pricing", label: "Pricing", child: [] },
+  {
+    href: "/resources", label: "Resources", child: [
+      { href: "/docs", label: "Docs", child: [] },
+      { href: "/blogs", label: "Blog", child: [] },
+      { href: "/api", label: "API", child: [] },
+    ]
+  },
+  { href: "/about", label: "About", child: [] },
+  { href: "/contact", label: "Contact", child: [] },
 ];
 
 export default function Header() {
   return (
-    <header className="bg">
+    <header className="bg-white">
       <nav className="flex items-center justify-between gap-6 px-24 py-4 text-sm font-medium bg-white">
         {/* Left Section */}
         <ul className="flex w-2/3 items-center justify-between gap-6">
           {/* Logo */}
           <li>
             <button
-              className="flex items-center gap-2 text-xl font-semibold text-black"
+              className="flex items-center gap-2 text-base font-semibold text-black"
             >
-              <TbCopyCheckFilled className="size-8" />
-              trako
+              <TbCopyCheckFilled className="size-6" />
+              IssueTracker
             </button>
           </li>
 
           {/* Main Navigation */}
           <li>
-            <ul className="flex items-center gap-6 rounded-full bg-black/5 px-8 py-3 text-black">
-              {mainLinks.map(({ href, label }) => (
-                <li key={label}>
+            <ul className="flex items-center gap-6 text-black">
+              {mainLinks.map(({ href, label, child }) => (
+                <li key={label} className="relative group">
                   <Link
                     href={href}
-                    className="hover:text-blue-600 transition-colors"
+                    className={`hover:text-blue-600 group-hover:text-blue-600 transition-colors`}
                   >
                     {label}
                   </Link>
+                  {child.length > 0 && (
+                    <ul className="bg-transparent absolute left-0 z-10 w-48 rounded-xl border border-gray-200 hidden group-hover:block">
+                      {child?.map(({ href, label }) => (
+                        <li key={label}>
+                          <Link
+                            href={href}
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          >
+                            {label}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               ))}
             </ul>
@@ -47,7 +68,15 @@ export default function Header() {
         <ul className="flex items-center gap-6">
           <li>
             <Link
-              href="/"
+              href={"signin"}
+              className="hover:text-blue-600 transition-colors"
+            >
+              Sign In
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/signup"
               className="rounded-full bg-blue-600/90 px-6 py-3 text-white transition-colors hover:bg-blue-600"
             >
               Get Started
