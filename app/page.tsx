016abc94d540626e2642314d7df77b9331ad9bf5
Haris Amjad <harisamjad56@gmail.com>
2025-09-09 01:25:09 +0500

    debug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,40 @@
 import { createClient } from '@/utils/supabase/server';
+import { Suspense } from 'react';
 
-export default async function Home() {
-  // const supabase = await createClient();
-  // const { data: issues } = await supabase.from("issues").select();
-  const issues = await getIssues();
+const ListIssues = async () => {
+  const issues = await getServerIssues();
 
   return (
-    <>
+    <div>
       {issues?.map((issue, index) => (
         <div key={index}>
           <h2>{issue.title}</h2>
           <p>{issue.description}</p>
         </div>
       ))}
-    </>
+    </div>
   )
 }
 
 // Server Component
 // Fetch data directly from Supabase and render it
 // No need for an API route to fetch issues
-const getIssues = async () => {
+const getServerIssues = async () => {
   const supabase = await createClient();
-  const { data, error } = await supabase
-    .from("issues")
-    .select()
-    .order("created_at", { ascending: false });
+  const { data, error } = await supabase.from("issues").select().order("created_at", { ascending: false });
 
   if (error) console.error("Error fetching issues:", error);
 
   return data;
 };
+
+export default async function Home() {
+  return (
+    <main>
+      <h1>Issue Tracker</h1>
+      <Suspense fallback={<div>Loading issues...</div>}>
+        <ListIssues />
+      </Suspense>
+    </main>
+  );
+}
