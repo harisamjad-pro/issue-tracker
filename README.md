9d09ba9b52c5d2ad07455469e2e7139a8a9e4fe8
Haris Amjad <harisamjad56@gmail.com>
2025-09-09 01:08:57 +0500

    Test Server Component to get data

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ A system to track bugs and assign to other users.
  1. To create a new Next.js app, run: `npx create-next-app@latest`
  2. Install React Icons: `npm i react-icons` or `npm install react-icons`
  3. Install Supabase: `npm install @supabase/supabase-js`
+ 4. Install Supabase Server: `npm install @supabase/ssr`
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Header from "@/components/Header";
+import { createClient } from '@/utils/supabase/server';
 
-export default function Home() {
+export default async function Home() {
+  // const supabase = await createClient();
+  // const { data: issues } = await supabase.from("issues").select();
+  const issues = await getIssues();
+
   return (
-    <div>
-      <Header />
-    </div>
-  );
+    <>
+      {issues?.map((issue, index) => (
+        <div key={index}>
+          <h2>{issue.title}</h2>
+          <p>{issue.description}</p>
+        </div>
+      ))}
+    </>
+  )
 }
+
+// Server Component
+// Fetch data directly from Supabase and render it
+// No need for an API route to fetch issues
+const getIssues = async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("issues")
+    .select()
+    .order("created_at", { ascending: false });
+
+  if (error) console.error("Error fetching issues:", error);
+
+  return data;
+};
