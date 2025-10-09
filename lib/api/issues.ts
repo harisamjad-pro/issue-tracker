29f839694ae1023cc6031412e7dc2a3bd73197a5
Haris Amjad <harisamjad56@gmail.com>
2025-10-10 04:22:17 +0500

    g

--- lib/api/issues.ts
+++ lib/api/issues.ts
@@ -1,10 +1,9 @@
 import { Issues } from "../types/issues";
 
-const API_BASE_URL = "http://localhost:3000/api/issues";
+const API_BASE_URL = "api/issues";
 
 export const IssueGetAPI = async (): Promise<Issues[]> => {
-  // const response = await fetch(API_BASE_URL, { method: "GET", cache: "no-store" });
-  const response = await fetch(API_BASE_URL, { cache: "no-store" });
+  const response = await fetch(API_BASE_URL, { method: "GET", cache: "no-store" });
   if (!response.ok) throw new Error(`Failed fetching data request :( ${response.status} ${response.statusText}`);   // Handle non-2xx HTTP responses
   return response.json();
 }
