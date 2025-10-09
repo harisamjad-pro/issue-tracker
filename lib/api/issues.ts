f343c0e1d0cf2144ee5ead9350fa84e210fdb966
Haris Amjad <harisamjad56@gmail.com>
2025-10-10 04:23:32 +0500

    d

--- lib/api/issues.ts
+++ lib/api/issues.ts
@@ -1,6 +1,6 @@
 import { Issues } from "../types/issues";
 
-const API_BASE_URL = "api/issues";
+const API_BASE_URL = "/api/issues";
 
 export const IssueGetAPI = async (): Promise<Issues[]> => {
   const response = await fetch(API_BASE_URL, { method: "GET", cache: "no-store" });
