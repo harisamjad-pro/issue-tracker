87594caf7738c917436ac57ee93d0db86cc74308
Haris Amjad <harisamjad56@gmail.com>
2025-10-10 16:53:28 +0500

    ne

--- lib/api/issues.ts
+++ lib/api/issues.ts
@@ -8,16 +8,14 @@ const API_BASE_URL = "http://localhost:3000/api/issues";
 //   return response.json();
 // }
 
-export const IssueGetAPI = async (): Promise<Issues[]> => (await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/issues`, { method: "GET", cache: "no-store" })).json();
+export const IssueGetAPI = async (): Promise<Issues[]> =>
+  (await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/issues`, { method: "GET", cache: "no-store" })).json();
 
-export const IssuePostAPI = async (data: Issues) => {
-  return await fetch(API_BASE_URL, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data), });
-}
+export const IssuePostAPI = async (data: Issues) =>
+  await fetch(API_BASE_URL, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data), });
 
-export const IssuePutAPI = async (data: Issues) => {
-  return await fetch(`${API_BASE_URL}/${data.id}`, { method: "PUT", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data), });
-}
+export const IssuePutAPI = async (data: Issues) =>
+  await fetch(`${API_BASE_URL}/${data.id}`, { method: "PUT", headers: { "Content-Type": "application/json" }, body: JSON.stringify(data), });
 
-export const IssueDeleteAPI = async (id: string) => {
-  return await fetch(`${API_BASE_URL}/${id}`, { method: "DELETE", cache: "no-store" });
-}
+export const IssueDeleteAPI = async (id: string) =>
+  await fetch(`${API_BASE_URL}/${id}`, { method: "DELETE", cache: "no-store" });
