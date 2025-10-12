ed8e3db59dbde28bc707a708d51e5a2840299850
Haris Amjad <harisamjad56@gmail.com>
2025-10-12 14:44:27 +0500

    test

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@/lib/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function DELETE(req: NextRequest, context: {params: Promise<{ id: string }>}) {
+export async function DELETE(_: NextRequest, context: {params: Promise<{ id: string }>}) {
   const { id } = await context.params;
   const supabase = await createClient();
   const { error } = await supabase.from("issues").delete().eq("id", id);
